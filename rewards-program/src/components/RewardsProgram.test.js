7cbaae4cc7ec7bd3d5bc38a23c123ef5f34d8239
eowdeveloper <madeline.eow@gmail.com>
2023-02-28 13:59:31 -0500

    unit testing

--- rewards-program/src/components/RewardsProgram.test.js
+++ rewards-program/src/components/RewardsProgram.test.js
@@ -1,16 +1,19 @@
 import { render, screen, within } from "@testing-library/react";
-import { jest, test} from '@jest/globals';
+import { jest, test } from '@jest/globals';
 import RewardsProgram from "./RewardsProgram";
 import * as api from '../api/customerApis';
 
 describe("RewardsProgram", () => {
+  const transactions = [
+    { customer: "Elaina Jillian", date: "2022-01-01", amount: 120 },
+    { customer: "Elaina Jillian", date: "2022-01-15", amount: 50 },
+    { customer: "Nathan Neil", date: "2022-01-05", amount: 80 },
+    { customer: "Nathan Neil", date: "2022-01-20", amount: 120 },
+  ];
+
+  // uses spyOn to mock the 'fetchTransactions' method of the api object and resolve it with an array of transaction data.
   beforeEach(() => {
-    jest.spyOn(api, 'fetchTransactions').mockResolvedValue([
-      { customer: "Elaina Jillian", date: "2022-01-01", amount: 120 },
-      { customer: "Elaina Jillian", date: "2022-01-15", amount: 50 },
-      { customer: "Nathan Neil", date: "2022-01-05", amount: 80 },
-      { customer: "Nathan Neil", date: "2022-01-20", amount: 120 },
-    ]);
+    jest.spyOn(api, 'fetchTransactions').mockResolvedValue(transactions);
   });
 
   test("renders loading message while transactions are being fetched", async () => {
@@ -19,22 +22,7 @@ describe("RewardsProgram", () => {
     await screen.findByText("Reward Points");
   });
 
-  test("renders table with reward points after transactions are fetched", async () => {
-    const transactions = [
-      {
-        customer: "Nathan Neil",
-        date: "2022-01-01",
-        amount: 120,
-      },
-      {
-        customer: "Elaina Jillian",
-        date: "2022-01-20",
-        amount: 140,
-      },
-    ];
-  
-    jest.spyOn(api, "fetchTransactions").mockResolvedValue(transactions);
-  
+  test("renders table with reward points after transactions are fetched", async () => {  
     render(<RewardsProgram />);
     await screen.findByText("Reward Points");
   
@@ -44,11 +32,11 @@ describe("RewardsProgram", () => {
     expect(rows[0]).toHaveTextContent("Customer");
     expect(rows[0]).toHaveTextContent("Month");
     expect(rows[0]).toHaveTextContent("Reward Points");
-    expect(rows[1]).toHaveTextContent("Nathan Neil");
+    expect(rows[1]).toHaveTextContent("Elaina Jillian");
     expect(rows[1]).toHaveTextContent("2022-01");
     expect(rows[1]).toHaveTextContent("90");
-    expect(rows[2]).toHaveTextContent("Elaina Jillian");
+    expect(rows[2]).toHaveTextContent("Nathan Neil");
     expect(rows[2]).toHaveTextContent("2022-01");
-    expect(rows[2]).toHaveTextContent("130");
+    expect(rows[2]).toHaveTextContent("120");
   });
 });
